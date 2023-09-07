1379b18c06139ad4709d6ac8e6a53752c1d11d94
Ehab-Ezzat <ehab.toahmy96@gmail.com>
2023-09-07 09:36:24 +0300

    added interceptor to send the token in header and handle http requests error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from "./core/components/navbar.component";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {GlobalInterceptor} from "./core/interceptors/global.interceptor";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import {HttpClientModule} from "@angular/common/http";
     NavbarComponent,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: GlobalInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
